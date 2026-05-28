971e77a22fb847adc204c1e670115f9a9ccd3f9d
Rakesh Nair <crucifyrakesh@gmail.com>
2026-05-28 21:47:13 +0530

    changes in properties

--- src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -1,14 +1,13 @@
 package com.learnkafka.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.learnkafka.domain.LibraryEventType;
 import com.learnkafka.producer.LibraryEventsProducer;
+import jakarta.annotation.Nullable;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.learnkafka.domain.LibraryEvent;
 
@@ -36,10 +35,35 @@ public class LibraryEventsController {
 		//libraryEventsProducer.sendLibraryEvent_approach3(libraryEvent);
 		log.info("Library Event : {} ",libraryEvent);
 		return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
-		
-		
 	}
-	
+
+	@PutMapping("/v1/libraryevent")
+	public ResponseEntity<?> updateLibraryEvent(
+			@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException {
+
+		final ResponseEntity<String> BAD_REQUEST = validateLibraryEvent(libraryEvent);
+		if (BAD_REQUEST != null) return BAD_REQUEST;
+
+		//Post Library Event to Kafka
+		//libraryEventsProducer.sendLibraryEvent(libraryEvent);
+		//libraryEventsProducer.sendLibraryEventApproach2(libraryEvent);
+		libraryEventsProducer.sendLibraryEvent_approach3(libraryEvent);
+		log.info("Library Event : {} ",libraryEvent);
+		return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+	}
+
+	@Nullable
+	private static ResponseEntity<String> validateLibraryEvent(LibraryEvent libraryEvent) {
+		if(libraryEvent.libraryEventId() == null){
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please pass the LibraryEventId");
+		}
+
+		if(!libraryEvent.libraryEventType().equals(LibraryEventType.UPDATE)){
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Only UPDATE event type is supported");
+		}
+		return null;
+	}
+
 	@GetMapping("/v1/libraryevent/test")
 	public String testLibraryEvent(){
 				
